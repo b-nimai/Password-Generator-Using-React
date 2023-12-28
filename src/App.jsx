f83398ed2775ef12aa4e2440c6e94584ee94bfa0
b-nimai <nimaibarman4978@gmail.com>
2023-12-28 15:53:24 +0530

    Template added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  
+
+  return (
+    <div>
+      <h1 className="text-4xl text-green-500 text-center">Hello , Wellcome to home</h1>
+    </div>
+  )
+}
+
+export default App
